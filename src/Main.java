a96c0ec1998fcdd9b3436a846e18ed6060d161ed
user <alexust11@yandex.ru>
2022-09-27 19:56:31 +0700

    ООП.Инкапсуляция. Д/з 2. Задание 1

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,27 @@ public class Main {
     public static void main(String[] args) {
         // Домашка 3
         // Задача 1
-        Human max=new Human("",-1980,"Минск","");
-        Human anna=new Human("Аня",29,"Москва","методист образовательных программ");
-        Human ket=new Human("Катя",28,"Калининград","продакт-менеджер");
-        Human artem=new Human("Артем",27,"Москва","директор по развитию бизнеса");
+       // Human max=new Human("",-1980,"Минск","");
+        //Human anna=new Human("Аня",29,"Москва","методист образовательных программ");
+        //Human ket=new Human("Катя",28,"Калининград","продакт-менеджер");
+        //Human artem=new Human("Артем",27,"Москва","директор по развитию бизнеса");
 
 
 
-        System.out.println(max.toString());
-        System.out.println(anna.toString());
-        System.out.println(ket.toString());
-        System.out.println(artem.toString());
+       // System.out.println(max.toString());
+        //System.out.println(anna.toString());
+       // System.out.println(ket.toString());
+        //System.out.println(artem.toString());
         System.out.println("_________________Задание 2________________________________________________");
         // Задание 2
         Car lada=new Car("Lada","Lada Grande",1.7f,"Желтый",2015,"Россия", "автомат","седан", "a123aа124", 6,"летняя"  );
-        //Car audi=new Car("Audi","Audi A8 50 L TDI quattro",3.0f,"Черный",2020,"Германия");
+        Car audi=new Car("Audi","Audi A8 50 L TDI quattro",3.0f,"Черный",2020,"Германия", "автомат", "универсал","у678см122",9,"зимняя");
         //Car bmw=new Car("BMW","",0,"Черный",0,"");
         //Car kia=new Car("Kia","Kia Sportage 4",2.4f,"Красный",2018,"Южная корея");
         //Car hyundai=new Car("Hyundai","Hyundai Avante",1.6f,"Оранжевый",2016,"Южная корея");
 
-        System.out.println(lada.toString());
-        //System.out.println(audi.toString());
+        System.out.println(lada);
+        System.out.println(audi);
         //System.out.println(bmw.toString());
         //System.out.println(kia.toString());
         //System.out.println(hyundai.toString());
